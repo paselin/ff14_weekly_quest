b6444390c997b0e1d22f26b70746877a8c15f526
katayama <katayama@logiclogic.jp>
2019-07-23 15:19:53 +0900

    更新日を表示するように

--- docs/js/main.js
+++ docs/js/main.js
@@ -10,6 +10,23 @@ function loadCookies(){
             e.checked = true;
         }
     });
+
+    target = new Date();
+    nextTuesDayDiff = target.getDay() - 2 + 7;
+    if (nextTuesDayDiff == 7) {
+        // 今日が火曜日の場合、今の時間を確認する
+        // 17:00前ならdiffを0に、17:00を超えていればdiffはそのまま
+        if (target.getHours() < 17){
+            nextTuesDayDiff = 0;
+        }
+    }
+    target.setDate(target.getDate() + nextTuesDayDiff);
+    target.setHours(17);
+    target.setMinutes(0);
+    target.setSeconds(0);
+    target.setMilliseconds(0);
+    let e = document.getElementsByName("nextUpdate")[0];
+    e.textContent = dateFormat(target);
 }
 
 // Cookieのセット時はexpiredを日付指定したいため、各情報として上書きする
@@ -44,3 +61,15 @@ function setCookies(){
         Cookies.set(element, value, { expires: target });
     });
 }
+
+// dateFormat 関数の定義
+function dateFormat(date) {
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    var d = date.getDate();
+    var h = date.getHours();
+    var mm = date.getMinutes();
+    mm = ('0' + mm).slice(-2);
+    // フォーマット整形済みの文字列を戻り値にする
+    return y + '年' + m + '月' + d + '日 ' + h + ":"+ mm;
+  }
