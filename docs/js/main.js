3d77e602f16b4db4116b10ef0992c4f731552524
katayama <katayama@logiclogic.jp>
2019-07-31 17:32:41 +0900

    トークン対応

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,74 +1,86 @@
+let HIGHEST_TOKEN_LIMIT = 450
+let MIDDLE_TOKEN_LIMIT = 900
+
 function loadCookies(){
     // 起動時にCookieを取得する
     // その取得したCookieの状態によってcheckedを切り替える
-    datas = ["loyalcustomers","doma","eden1","eden2","eden3","eden4","kuro"];
+    datas = ["token_fantasy","loyalcustomers","doma","eden1","eden2","eden3","eden4","kuro"];
     cookies = Cookies.get();
     datas.forEach(element => {
         let cookie = Cookies.get(element);
         if (cookie != undefined && cookie == 1) {
-            var e = document.getElementsByName(element)[0];
+            let e = document.getElementsByName(element)[0];
             e.checked = true;
         }
     });
 
-    target = new Date();
-    nextTuesDayDiff = target.getDay() - 2 + 7;
-    if (nextTuesDayDiff == 7) {
-        // 今日が火曜日の場合、今の時間を確認する
-        // 17:00前ならdiffを0に、17:00を超えていればdiffはそのまま
-        if (target.getHours() < 17){
-            nextTuesDayDiff = 0;
-        }
-    }
-    target.setDate(target.getDate() + nextTuesDayDiff);
-    target.setHours(17);
-    target.setMinutes(0);
-    target.setSeconds(0);
-    target.setMilliseconds(0);
+    target = calcNextResetDay(new Date());
     let e = document.getElementsByName("nextUpdate")[0];
     e.textContent = "次回更新は" + dateFormat(target) + "です。";
 }
 
-// Cookieのセット時はexpiredを日付指定したいため、各情報として上書きする
-// でもCookieってドメインごとに一括で持つんじゃなかったっけ
-
 function setCookies(){
-    var cookies = new Cookies()
-    var cookieStr = "";
+    let cookies = new Cookies()
+    let cookieStr = "";
     // expireするタイミング(=火曜日17:00)を作る
-    target = new Date();
-    nextTuesDayDiff = target.getDay() - 2 + 7;
-    if (nextTuesDayDiff == 7) {
-        // 今日が火曜日の場合、今の時間を確認する
-        // 17:00前ならdiffを0に、17:00を超えていればdiffはそのまま
-        if (target.getHours() < 17){
-            nextTuesDayDiff = 0;
-        }
-    }
-    target.setDate(target.getDate() + nextTuesDayDiff);
-    target.setHours(17);
-    target.setMinutes(0);
-    target.setSeconds(0);
-    target.setMilliseconds(0);
+    target = calcNextResetDay(new Date());
     let expireDate = target.toGMTString();
     // 状態の取得
     datas.forEach(element => {
-        let e = document.getElementsByName(element)[0].checked;
-        var value = 0;
-        if (e){
+        let value = 0;
+        let form = document.getElementsByName(element)[0];
+        if (form != null && form.type == "text" && form.name == "token_fantasy"){
+            value = validate(form.value,0,HIGHEST_TOKEN_LIMIT);
+            form.value = validate(form.value,0,HIGHEST_TOKEN_LIMIT);
+        } else if (form.checked){
             value = 1;
         }
         Cookies.set(element, value, { expires: target });
     });
 }
 
+function calcNextResetDay(date){
+    let retDate = date;
+    let nextResetDayDiff = 2 - retDate.getDay(); // リセ日は現在火曜日
+    if (nextResetDayDiff < 0){
+        nextResetDayDiff = nextResetDayDiff + 7;
+    }
+    if (nextResetDayDiff == 0){
+        // 今日が火曜日の場合、今の時間を確認する
+        // 17:00前ならdiffを0に、17:00を超えていればdiffはそのまま
+        if (target.getHours() < 17){
+            nextTuesDayDiff = 0;
+        }
+    }
+    retDate.setDate(retDate.getDate() + nextResetDayDiff);
+    retDate.setHours(17);
+    retDate.setMinutes(0);
+    retDate.setSeconds(0);
+    retDate.setMilliseconds(0);
+    return retDate;
+}
+
+function validate(value,minVal,maxVal){
+    let retVal =  value;
+    if (isNaN(retVal)){
+        return minVal;
+    }
+    if (value < minVal){
+        retVal = minVal;
+    }
+    if (value > maxVal){
+        retVal = maxVal;
+    }
+    return retVal;
+}
+
 // dateFormat 関数の定義
 function dateFormat(date) {
-    var y = date.getFullYear();
-    var m = date.getMonth() + 1;
-    var d = date.getDate();
-    var h = date.getHours();
-    var mm = date.getMinutes();
+    let y = date.getFullYear();
+    let m = date.getMonth() + 1;
+    let d = date.getDate();
+    let h = date.getHours();
+    let mm = date.getMinutes();
     mm = ('0' + mm).slice(-2);
     // フォーマット整形済みの文字列を戻り値にする
     return y + '年' + m + '月' + d + '日 ' + h + ":"+ mm;
